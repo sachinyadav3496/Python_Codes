ef47a24ea2f31799cb3645552bf72029285f7aad
sachinyadav3496 <sachinyadav3496@gmail.com>
2018-08-25 09:31:11 +0530

    added code for last class

---
+++ batch_5pm/my.py
@@ -0,0 +1,15 @@
+name = input("Enter your name : " )
+age = input("Enter your age : ")
+country = input("Enter your country : ")
+language = input("Enter your language : ")
+
+info = { 
+        'name'  : name,
+        'age' : age,
+        'country' : country,
+        'language' : language,
+        }
+
+print("Here is your info dictionary : ")
+
+print(info)
