4701adea542a2110bb05ced05cfa1c0e44ad3909
sachinyadav3496 <sachinyadav3496@gmail.com>
2018-10-05 18:49:38 +0530

    added code for recursion and scoping

---
+++ batch_5pm/scope1.py
@@ -0,0 +1,17 @@
+x = 5
+y = 10
+
+
+def add(a,b):
+    p = 5
+    q = 10
+    return a*p+q*b
+
+print(x)
+print(y)
+
+r = add(x,y)
+
+print(p)
+print(q)
+print(r)
---
+++ batch_5pm/scope3.py
@@ -0,0 +1,16 @@
+
+x  = 5
+y = 10 
+
+def swap(p,q):
+    x = q
+    y = p 
+
+print("before swap x = ",x)
+print("before swap y = ",y)
+
+swap(x,y)
+
+print("after swap x = ",x)
+print("after swap y = ",y)
+
