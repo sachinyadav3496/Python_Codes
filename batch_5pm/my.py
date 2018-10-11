afe45252eb177dfe145aedaf0cb2f82b476a4fa3
sachinyadav3496 <sachinyadav3496@gmail.com>
2018-10-11 22:08:56 +0530

    added code for classes

--- batch_5pm/my.py
+++ batch_5pm/my.py
@@ -1,15 +1,17 @@
-name = input("Enter your name : " )
-age = input("Enter your age : ")
-country = input("Enter your country : ")
-language = input("Enter your language : ")
+print("\t\tmodule name = ",__name__)
+if __name__ == "__main__" : 
+    name = input("Enter your name : " )
+    age = input("Enter your age : ")
+    country = input("Enter your country : ")
+    language = input("Enter your language : ")
 
-info = { 
-        'name'  : name,
-        'age' : age,
-        'country' : country,
-        'language' : language,
-        }
+    info = { 
+            'name'  : name,
+            'age' : age,
+            'country' : country,
+            'language' : language,
+            }
 
-print("Here is your info dictionary : ")
+    print("Here is your info dictionary : ")
 
-print(info)
+    print(info)
